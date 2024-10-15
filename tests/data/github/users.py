7be74bc3b3d3c942ce8ce26d1e4a219e36614939
Daniel Brauer <dbrauer@etsy.com>
2024-10-15 10:37:04 -0400

    enterprise owner test data

--- tests/data/github/users.py
+++ tests/data/github/users.py
@@ -24,6 +24,46 @@ GITHUB_USER_DATA = [
     },
 ]
 
+# Note the subtle differences between owner data and user data:
+# 1. owner data does not include a `hasTwoFactorEnabled` field (it in unavailable in the GraphQL query for these owners)
+# 2. an `organizationRole` field instead of a `role` field.  For user data, membership in the queried org
+#    is assumed.  The owner data, membership is not assumed, so there is an 'UNAFFILIATED' value for owners who are
+#    not also users in an organization.  In this list, the 'OWNER' organizationRole matches the 'ADMIN' role in the
+#    user data.  Similarly, the 'DIRECT_MEMBER' organizationRole matches the 'MEMBER' role.
+GITHUB_ENTERPRISE_OWNER_DATA = [ # TODO put in real fake values for testing
+    {
+        'node': {
+            'url': 'https://example.com/kbroflovski',
+            'login': 'kbroflovski',
+            'name': 'Kyle Broflovski',
+            'isSiteAdmin': False,
+            'email': 'kbroflovski@example.com',
+            'company': 'South Park Elementary',
+        },
+        'organizationRole': 'UNAFFILIATED',
+    }, {
+        'node': {
+            'url': 'https://example.com/bjsimpson',
+            'login': 'bjsimpson',
+            'name': 'Bartholomew Simpson',
+            'isSiteAdmin': False,
+            'email': 'bjsimpson@example.com',
+            'company': 'Simpson Residence',
+        },
+        'organizationRole': 'DIRECT_MEMBER',
+    }, {
+        'node': {
+            'url': 'https://example.com/lmsimpson',
+            'login': 'lmsimpson',
+            'name': 'Lisa Simpson',
+            'isSiteAdmin': False,
+            'email': 'lmsimpson@example.com',
+            'company': 'Simpson Residence',
+        },
+        'organizationRole': 'OWNER',
+    },
+]
+
 GITHUB_ORG_DATA = {
     'url': 'https://example.com/my_org',
     'login': 'my_org',
